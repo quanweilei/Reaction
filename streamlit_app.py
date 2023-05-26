231f93eee2fa3a75245d1e68cab92a25f49a4828
Quanwei Lei <108092123+quanweilei@users.noreply.github.com>
2023-05-26 13:00:23 -0700

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,9 @@
-# streamlit app for the reaction project
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# streamlit app for the reaction project for Dr. Florian Goeltl University of Arizona
+# first open UAR-v1.0xlsx using pandas
+# then create a dataframe with the data
+st.title('University of Arizona Reaction Database')
+
